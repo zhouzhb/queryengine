466bc2d6df98c0b238c2638446c482c303490062
Valeriy Trofimov <ValTrofFuture@gmail.com>
2019-07-01 14:06:39 -0700

    add Calcite->Thrift->MySQL query test

--- src/test/java/com/huawei/cloudsop/us/queryengine/SchemaTest.java
+++ src/test/java/com/huawei/cloudsop/us/queryengine/SchemaTest.java
@@ -10,12 +10,25 @@ import org.junit.Test;
 public class SchemaTest {
 
     /**
-     * Tests connection to Thrift JDBC server running on Spark: https://spark.apache.org/docs/latest/sql-distributed-sql-engine.html
-     * Precondition for this test is a VM set up using steps at: https://github.com/zhouzhb/queryengine/blob/master/doc/Spark-VM.txt
+     * Tests Calcite->Thrift->Hive querying: https://spark.apache.org/docs/latest/sql-distributed-sql-engine.html.
+     * Precondition for this test is a VM set up using steps at: https://github.com/zhouzhb/queryengine/blob/master/doc/Spark-VM.txt.
      */
-    @Test public void testThriftScheme() {
+    @Test public void testThriftHive() {
         QueryController controller = new QueryController();
         String query = "select * from \"thrift\".\"test\"";
+        QueryResult res = controller.query(query);
+        System.out.println("result: " + res.getContent());
+    }
+
+    /**
+     * Tests Calcite->Thrift->MySQL querying.
+     * Preconditions for this test are:
+     * - VM is set up using steps at: https://github.com/zhouzhb/queryengine/blob/master/doc/Spark-VM.txt.
+     * - MySQL is set up using steps at: https://community.hortonworks.com/articles/4671/sparksql-jdbc-federation.html.
+     */
+    @Test public void testThriftMySql() {
+        QueryController controller = new QueryController();
+        String query = "select * from \"thrift\".\"mysql_federated_sample\"";
         QueryResult res = controller.query(query);
         System.out.println("result: " + res.getContent());
     }
